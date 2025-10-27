522b4907f075786eeee155be1d0fec0f472654ad
nestnic supplies <nestnicsupplies@gmail.com>
2025-10-27 04:05:51 +0000

    Try fixing this error: `Console Error: In HTML, <a> cannot be a descenda

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -43,7 +43,7 @@ export function MainNav() {
       <NavigationMenu>
         <NavigationMenuList>
           <NavigationMenuItem>
-            <Link href="/" passHref>
+            <Link href="/" legacyBehavior passHref>
               <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), pathname === '/' ? 'bg-accent' : '')}>
                 <Terminal className="mr-2"/> Home
               </NavigationMenuLink>
@@ -66,14 +66,14 @@ export function MainNav() {
             </NavigationMenuContent>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="/guides" passHref>
+            <Link href="/guides" legacyBehavior passHref>
               <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), pathname === '/guides' ? 'bg-accent' : '')}>
                 <BookOpen className="mr-2"/> Guides
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="/about" passHref>
+            <Link href="/about" legacyBehavior passHref>
                 <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), pathname === '/about' ? 'bg-accent' : '')}>
                     About
                 </NavigationMenuLink>
@@ -86,15 +86,15 @@ export function MainNav() {
 }
 
 const ListItem = React.forwardRef<
-  React.ElementRef<typeof Link>,
-  React.ComponentPropsWithoutRef<typeof Link>
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
 >(({ className, title, children, href, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
         <Link
-          ref={ref}
           href={href!}
+          ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
             className
